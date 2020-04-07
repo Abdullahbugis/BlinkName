88b32e26a4da0f98545bd792b7017eeacc424abb
Abdullahbugis <49539706+Abdullahbugis@users.noreply.github.com>
2020-04-08 06:31:38 +1000

    Add files via upload

---
+++ morsecodefirstname.ino
@@ -0,0 +1,168 @@
+// Task 2.1 blinking my first name Abdullah 
+
+int ledPin = D7; // Blue led is connected to the argon on D7
+
+
+
+
+void setup() {
+
+  
+
+  pinMode(ledPin, OUTPUT);
+  
+
+}
+
+
+void loop() {
+  
+  
+  
+  // A
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(4000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+
+  
+  // B
+  
+  digitalWrite(ledPin, HIGH);
+  delay(4000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  
+  
+  
+ // D
+ 
+  digitalWrite(ledPin, HIGH);
+  delay(4000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000); 
+ 
+
+
+
+// U 
+
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(4000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  
+  
+  
+  // L
+  
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(4000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  
+  
+  // L
+  
+  
+   digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(4000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  
+  
+  
+ // A 
+ 
+ 
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(4000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+
+
+
+// H
+  
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  
+  
+
+  
+}
+
